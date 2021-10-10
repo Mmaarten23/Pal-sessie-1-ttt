f466fe6dbbb59fd4145baba8a0c6692f4aa1c801
Maarten Magits <mmaartendisc@gmail.com>
2021-10-10 21:51:14 +0200

    Improved test handling + Made file more readable.
    Signed-off-by: Maarten Magits <mmaartendisc@gmail.com>

--- provided_tests.py
+++ provided_tests.py
@@ -1,6 +1,10 @@
 from opgave_template import is_lege_matrix, eentonige_lijst, is_matrix, gelijke_rand_matrix, strip_matrix
 
 
+def printResult(actual, expected):
+    print("succeeded" if actual == expected else "failed")
+
+
 def runChecks():
     print("")
     print("====================================")
@@ -8,51 +12,79 @@ def runChecks():
     print("====================================")
     print("")
     print("-- is_lege_matrix --")
-    print(is_lege_matrix([]) is True)  # True
-    print(is_lege_matrix([[]]) is True)  # True
-    print(is_lege_matrix([[1, 1]]) is False)  # False
-    print(is_lege_matrix([[1, 2]]) is False)  # False
-    print("-- eentonige_lijst --")
-    print(eentonige_lijst([]) is True)  # True
-    print(eentonige_lijst([1]) is True)  # True
-    print(eentonige_lijst([1, 1]) is True)  # True
-    print(eentonige_lijst([1, 2]) is False)  # False
-    print(eentonige_lijst([1, '1']) is False)  # False
-    print("-- is_matrix --")
-    print(is_matrix([]) is True)  # True
+    printResult(is_lege_matrix([]), True)
+    printResult(is_lege_matrix([[]]), True)
+    printResult(is_lege_matrix([[1, 1]]), False)
+    printResult(is_lege_matrix([[1, 2]]), False)
 
-    print(is_matrix([[]]) is True)  # True
-    print(is_matrix([[1]]) is True)  # True
-    print(is_matrix([[1, 'a']]) is True)  # True
-    print(is_matrix([[1, 'a'], [True, 1.2]]) is True)  # True
-    print(is_matrix([[1, 'a'], [True, 1.2], ["hello"]]) is False)  # False
+    print("-- eentonige_lijst --")
+    printResult(eentonige_lijst([]), True)
+    printResult(eentonige_lijst([1]), True)
+    printResult(eentonige_lijst([1, 1]), True)
+    printResult(eentonige_lijst([1, 2]), False)
+    printResult(eentonige_lijst([1, '1']), False)
+
+    print("-- is_matrix --")
+    printResult(is_matrix([]), True)
+    printResult(is_matrix([[]]), True)
+    printResult(is_matrix([[1]]), True)
+    printResult(is_matrix([[1, 'a']]), True)
+    printResult(is_matrix([[1, 'a'], [True, 1.2]]), True)
+    printResult(is_matrix([[1, 'a'], [True, 1.2], ["hello"]]), False)
+
     print("-- gelijke_rand_matrix --")
-    print(gelijke_rand_matrix([[1, 'a'], [True, 1.2]]) is False)  # False
-    print(gelijke_rand_matrix([[1, 1, 1, 1],
-                               [1, 1.2, True, 1],
-                               [1, 1, 2, 1]])
-          is False)  # False
-    print(gelijke_rand_matrix([[1, 1, 1, 1],
-                               [1, 1.2, True, 1],
-                               [1, 1, 1, 1]])
-          is True)  # True
-    print(gelijke_rand_matrix([['a', 'a', 'a', 'a'],
-                               ['a', 1.2, True, 'a'],
-                               ['a', 1, 1, 'a'],
-                               ['a', 'a', 'a', 'a']])
-          is True)  # True
-    print(gelijke_rand_matrix([['a', 'a', 'a', 'a'],
-                               ['a', 1.2, True, 'a'],
-                               ['a', 1, 1, 'a'],
-                               ['a', 'b', 'a', 'a']])
-          is False)  # False
+    printResult(
+        gelijke_rand_matrix(
+            [[1, 'a'],
+             [True, 1.2]]),
+        False
+    )
+    printResult(
+        gelijke_rand_matrix(
+            [[1, 1, 1, 1],
+             [1, 1.2, True, 1],
+             [1, 1, 2, 1]]),
+        False
+    )
+    printResult(
+        gelijke_rand_matrix(
+            [[1, 1, 1, 1],
+             [1, 1.2, True, 1],
+             [1, 1, 1, 1]]),
+        True
+    )
+    printResult(
+        gelijke_rand_matrix(
+            [['a', 'a', 'a', 'a'],
+             ['a', 1.2, True, 'a'],
+             ['a', 1, 1, 'a'],
+             ['a', 'a', 'a', 'a']]),
+        True
+    )
+    printResult(
+        gelijke_rand_matrix([['a', 'a', 'a', 'a'],
+                             ['a', 1.2, True, 'a'],
+                             ['a', 1, 1, 'a'],
+                             ['a', 'b', 'a', 'a']]),
+        False
+    )
     print("-- strip_matrix --")
-    print(strip_matrix([['a', 'a', 'a', 'a'],
-                        ['a', 1.2, True, 'a'],
-                        ['a', 1, 1, 'a'],
-                        ['a', 'b', 'a', 'a']]) == [[1.2, True], [1, 1]])  # [[1.2, True], [1, 1]]
-    print(strip_matrix([['a', 'a', 'a', 'a'],
-                        ['a', 1.2, True, 'a'],
-                        ['a', 'b', 'a', 'a']]) == [[1.2, True]])  # [[1.2, True]]
+    printResult(
+        strip_matrix(
+            [['a', 'a', 'a', 'a'],
+             ['a', 1.2, True, 'a'],
+             ['a', 1, 1, 'a'],
+             ['a', 'b', 'a', 'a']]),
+        [[1.2, True],
+         [1, 1]]
+    )
+
+    printResult(
+        strip_matrix(
+            [['a', 'a', 'a', 'a'],
+             ['a', 1.2, True, 'a'],
+             ['a', 'b', 'a', 'a']]),
+        [[1.2, True]]
+    )
     m = strip_matrix([[1, 1, 1, 1]])
-    print(m == [] or m == [[]])  # een lege matrix, [] of[[]]
+    printResult(m, [])
